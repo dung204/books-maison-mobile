4339665209520bfd86179182146c5b697f6c21a8
dung204 <acezombiev4@gmail.com>
2025-01-06 02:59:52 +0700

    chore: update types support for metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('@storybook/react-native/dist/metro/withStorybook').default} */
 const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = withStorybook(defaultConfig, {
